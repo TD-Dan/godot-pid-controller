144021983fb894e5c3393a61bdb6a5f433b37643
Dani Starborn <37656679+TD-Dan@users.noreply.github.com>
2023-05-19 09:11:40 +0300

    initial commit

---
+++ pd_controller.gd
@@ -0,0 +1,22 @@
+extends Object
+
+class_name PDController
+
+var setpoint = 0.0
+
+var Kp = 1.0
+var Kd = 1.0
+
+var previous_error = 0.0
+
+func set_pd(proportional:float, derivative:float):
+	Kp = proportional
+	Kd = derivative
+
+func step(measure:float, dt:float) -> float:
+	var ret = 0
+	var error = setpoint - measure
+	var derivative = (error - previous_error) / dt
+	ret = Kp * error + Kd * derivative
+	previous_error = error
+	return ret
---
+++ pid_controller.gd
@@ -0,0 +1,26 @@
+extends Object
+
+class_name PIDController
+
+var setpoint = 0.0
+
+var Kp = 1.0
+var Ki = 1.0
+var Kd = 1.0
+
+var previous_error = 0.0
+var integral = 0.0
+
+func set_pid(proportional:float, integrative:float, derivative:float):
+	Kp = proportional
+	Ki = integrative
+	Kd = derivative
+
+func step(measure:float, dt:float) -> float:
+	var ret = 0
+	var error = setpoint - measure
+	integral = integral + error * dt
+	var derivative = (error - previous_error) / dt
+	ret = Kp * error + Ki * integral + Kd * derivative
+	previous_error = error
+	return ret
