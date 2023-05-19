5621ba5d1d442bf364a41f54baf30dafd17a14b1
Daniel Herkert <daniel.herkert@gmail.com>
2023-05-19 11:24:20 +0300

    added reste function and docstrings

--- pd_controller.gd
+++ pd_controller.gd
@@ -7,16 +7,22 @@ var setpoint = 0.0
 var Kp = 1.0
 var Kd = 0.1
 
-var previous_error = 0.0
+var _previous_error = 0.0
 
 func set_pd(proportional:float, derivative:float):
 	Kp = proportional
 	Kd = derivative
 
+## Evaluate one step towards the setpoint
+## my_process_value -= my_pd_controller.step(my_process_value, delta_time)
 func step(measure:float, dt:float) -> float:
 	var ret = 0
 	var error = setpoint - measure
-	var derivative = (error - previous_error) / dt
+	var derivative = (error - _previous_error) / dt
 	ret = Kp * error + Kd * derivative
-	previous_error = error
+	_previous_error = error
 	return ret
+
+## Reset internal state to zero
+func reset():
+	_previous_error = 0.0
--- pid_controller.gd
+++ pid_controller.gd
@@ -8,19 +8,26 @@ var Kp = 1.0
 var Ki = 0.1
 var Kd = 0.01
 
-var previous_error = 0.0
-var integral = 0.0
+var _previous_error = 0.0
+var _integral = 0.0
 
 func set_pid(proportional:float, integrative:float, derivative:float):
 	Kp = proportional
 	Ki = integrative
 	Kd = derivative
 
+## Evaluate one step towards the setpoint
+## my_process_value -= my_pd_controller.step(my_process_value, delta_time)
 func step(measure:float, dt:float) -> float:
 	var ret = 0
 	var error = setpoint - measure
-	integral = integral + error * dt
-	var derivative = (error - previous_error) / dt
-	ret = Kp * error + Ki * integral + Kd * derivative
-	previous_error = error
+	_integral = _integral + error * dt
+	var derivative = (error - _previous_error) / dt
+	ret = Kp * error + Ki * _integral + Kd * derivative
+	_previous_error = error
 	return ret
+
+## Reset internal state to zero
+func reset():
+	_previous_error = 0.0
+	_integral = 0.0
